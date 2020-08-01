4036659a3c5e37e3d99f5c380b1d3b3e63ce777a
takumi-19 <tkm19.k.univ@icloud.com>
2020-08-02 04:28:01 +0900

    edit scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_supporter!
+  # before_action :authenticate_supporter!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
