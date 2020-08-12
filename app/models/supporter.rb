8cd234f824cf6711862193eca39805d9b3102bba
takumi-19 <tkm19.k.univ@icloud.com>
2020-08-12 12:35:55 +0900

    update login & signup page

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,9 +1,9 @@
 class Supporter < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: { maximum: 10 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
 
   mount_uploader :avatar_image, ImageUploader
 
