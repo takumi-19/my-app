e507a21ee3826b317e48d72ce4a35fda05e37c84
takumi-19 <tkm19.k.univ@icloud.com>
2020-08-14 17:29:58 +0900

    add basic authentication #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  # http_basic_authenticate_with :name => ENV['admin4'], :password => ENV['add19'] if Rails.env == "production"
+
   before_action :authenticate_supporter!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,7 +10,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :team_id])
   end
-
-  http_basic_authenticate_with :name => ENV['admin4'], :password => ENV['add19'] if Rails.env == "production"
-
 end
